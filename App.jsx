ed8551cc7f1bccfe8511f9f473dc9f098d82e63a
gyusang <monet000414@naver.com>
2023-10-20 15:36:26 +0900

    redux 구현중

--- App.jsx
+++ App.jsx
@@ -6,6 +6,8 @@
  */
 
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import {
   SafeAreaView,
   ScrollView,
@@ -31,6 +33,7 @@ const Stack = createStackNavigator();
 
 function App(){
   return (
+    <Provider store={store}>
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName="main"
@@ -47,6 +50,7 @@ function App(){
           <Stack.Screen name="Reservation" component={Reservation} />
       </Stack.Navigator>
     </NavigationContainer>
+    </Provider>
   );
 }
 
